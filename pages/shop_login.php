7cc4ea907d8dafcb748d9850d9fe13ec8cabc4d5
anpen <aqua.7505@gmail.com>
2025-10-23 04:46:30 +0900

    Update 2025/10/23  4:46:30.38

--- pages/shop_login.php
+++ pages/shop_login.php
@@ -1,13 +1,4 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-require_once '../config/config.php';
-require_once '../config/database.php';
-require_once '../includes/functions.php';
-
-$db = new Database();
 $page_title = '店舗ログイン';
 $page_description = '店舗管理者のログインを行います。';
 
